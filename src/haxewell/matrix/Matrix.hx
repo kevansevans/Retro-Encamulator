537a428b7168640120218d63a00aa14a60f57643
Kaelan <kbeevans@gmail.com>
2019-02-05 02:26:50 -0700

    Update matrix

--- src/haxewell/matrix/Matrix.hx
+++ src/haxewell/matrix/Matrix.hx
@@ -13,9 +13,11 @@ class Matrix
 	public static var memory:Memory;
 	public static var graphics:GPU;
 	public static var sound:Sound;
+	public static var register:Register;
 	public function new(_rom:String) 
 	{
 		processor = new CPU();
+		register = new Register();
 		memory = new Memory();
 		memory.load(_rom);
 		graphics = new GPU();
